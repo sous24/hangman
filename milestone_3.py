38c6687dcb20769e5ed21df83099befecb6285ce
Sous Abraham <theaateam24@gmail.com>
2023-10-23 09:13:28 +0100

    adds milestone 4

--- milestone_3.py
+++ milestone_3.py
@@ -1,25 +1,25 @@
 ## H A N G M A N
 import random
 word_list = ["Grape", "Pineapple", "Mango", "Blueberry", "Banana"]
-random_word = random.choice(word_list)
+word_index = random.randint(0, len(word_list)-1)
 
-def ask_for_input():
-    while True:
-        guess = input("Choose a letter: ")
-        guess = guess
-        if len(guess) == 1 and guess.isalpha():
-            break
-        else:
-            print("Invalid letter. Please, enter a single alphabetical character.")
-    return guess
+print(word_index)
+# def check_guess(random_word, guess):
+#     guess = guess.lower()
+#     if guess in random_word:
+#         print("Good guess!", guess, "is in the word.")
+#     else:
+#         print("Sorry,", guess, "is not in the word. Try again.")
 
-guess = ask_for_input()
+# def ask_for_input():
+#     while True:
+#         guess = input("Choose a letter: ")
+#         if len(guess) == 1 and guess.isalpha():
+#             check_guess(random_word, guess)
+#             break
+#         else:
+#             print("Invalid letter. Please, enter a single alphabetical character.")
+#     return guess
+
+# ask_for_input()
     
-def check_guess(random_word, guess):
-    if guess in random_word:
-        print("Good guess!", guess, "is in the word.")
-    else:
-        print("Sorry,", guess, "is not in the word. Try again.")
-
-check_guess(random_word, guess)
-
